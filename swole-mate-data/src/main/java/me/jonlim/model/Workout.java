69dfd72a251fe4c1136662416a44dba92a8facf3
Jon Lim <jon.lim@utexas.edu>
2020-04-12 16:44:25 -0600

    Create multi-maven modules - data and web

---
+++ swole-mate-data/src/main/java/me/jonlim/model/Workout.java
@@ -0,0 +1,25 @@
+package me.jonlim.model;
+
+import java.time.LocalDate;
+
+public class Workout {
+
+  private LocalDate workoutDate;
+  private WorkoutType workoutType;
+
+  public LocalDate getWorkoutDate() {
+    return workoutDate;
+  }
+
+  public void setWorkoutDate(LocalDate workoutDate) {
+    this.workoutDate = workoutDate;
+  }
+
+  public WorkoutType getWorkoutType() {
+    return workoutType;
+  }
+
+  public void setWorkoutType(WorkoutType workoutType) {
+    this.workoutType = workoutType;
+  }
+}
---
+++ swole-mate-data/src/main/java/me/jonlim/model/WorkoutType.java
@@ -0,0 +1,14 @@
+package me.jonlim.model;
+
+public class WorkoutType {
+
+  private String workoutName;
+
+  public String getWorkoutName() {
+    return workoutName;
+  }
+
+  public void setWorkoutName(String workoutName) {
+    this.workoutName = workoutName;
+  }
+}
