05a5fee4261748946d86f5f007295a5a46863fca
Jon Lim <jon.lim@utexas.edu>
2020-04-13 20:19:57 -0600

    Prep pom.xml for release

---
+++ swole-mate-web/src/test/java/me/jonlim/swolemate/SwoleMateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jonlim.swolemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwoleMateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
